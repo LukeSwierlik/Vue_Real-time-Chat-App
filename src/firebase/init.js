16e77172bcd37bb0c68044beba2366c5e6a85597
LukeSwierlik <lukasz.swierlikowski@gmail.com>
2018-09-20 14:57:02 +0200

    Change config firebase.

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDSGuZlDuA2RGrinhAHMzQuqBmeVqyi3HE",
-    authDomain: "ninja-real-time-chat-app.firebaseapp.com",
-    databaseURL: "https://ninja-real-time-chat-app.firebaseio.com",
-    projectId: "ninja-real-time-chat-app",
-    storageBucket: "",
-    messagingSenderId: "168957292481"
+    // Config firebase
 };
 
 const firebaseApp = firebase.initializeApp(config);
